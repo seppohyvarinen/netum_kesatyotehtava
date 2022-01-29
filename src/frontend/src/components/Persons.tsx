2e954f826fefcf59f52d1e38ad0acad9223cfc10
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-29 15:50:49 +0200

    Add: Persons component now fetches(axios) all persons from database as useEffect

--- src/frontend/src/components/Persons.tsx
+++ src/frontend/src/components/Persons.tsx
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 export const Persons: React.FunctionComponent<{}> = () => {
+  const fetchAll = async () => {
+    try {
+      var response = await axios.get("/persons");
+
+      console.log(response.data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+  useEffect(() => {
+    fetchAll();
+  }, []);
   return <div className="PersonList">Here are the persons</div>;
 };
