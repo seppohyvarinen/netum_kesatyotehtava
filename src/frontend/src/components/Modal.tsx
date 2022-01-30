5da0e46449e343a106eadf15e9da7214656e15e2
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-01-30 16:42:24 +0200

    Add: inputs in Modal component now use state to store input data

--- src/frontend/src/components/Modal.tsx
+++ src/frontend/src/components/Modal.tsx
@@ -9,12 +9,33 @@ export const Modal: React.FunctionComponent<Props> = ({ setModalOpen }) => {
   const [firstName, setFirstName] = useState<string>("");
   const [age, setAge] = useState<number>();
 
+  const handleLastName = (e: React.FormEvent<HTMLInputElement>) => {
+    setLastName(e.currentTarget.value);
+    console.log(lastName);
+  };
+  const handleFirstName = (e: React.FormEvent<HTMLInputElement>) => {
+    setFirstName(e.currentTarget.value);
+    console.log(lastName);
+  };
+  const handleAge = (e: React.FormEvent<HTMLInputElement>) => {
+    setAge(e.currentTarget.valueAsNumber);
+    console.log(age);
+  };
+
   return (
     <div className="modalBG">
       <div className="Modal">
         <button className="exitBtn" onClick={() => setModalOpen(false)}>
           X
         </button>
+
+        <input type={"text"} onChange={handleLastName} value={lastName}></input>
+        <input
+          type={"text"}
+          onChange={handleFirstName}
+          value={firstName}
+        ></input>
+        <input type={"number"} onChange={handleAge} value={age}></input>
       </div>
     </div>
   );
