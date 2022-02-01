66290b18fc17c3bb0e61a2f143e1812a9dd4da8e
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-02-01 15:08:40 +0200

    Add: database function for editing persons

--- src/connections/connections.ts
+++ src/connections/connections.ts
@@ -60,6 +60,34 @@ let Connections = {
         }
       );
     }),
+  editPerson: (person: {
+    LastName: string;
+    FirstName: string;
+    Age: number;
+    ID: number;
+  }) =>
+    new Promise((resolve, reject) => {
+      pool.query(
+        "update Persons set LastName = " +
+          pool.escape(person.LastName) +
+          ", FirstName = " +
+          pool.escape(person.FirstName) +
+          ", Age = " +
+          pool.escape(person.Age) +
+          "where ID = " +
+          pool.escape(person.ID),
+        (err, Persons) => {
+          if (err) {
+            reject("data can't be edited for some reason, please try again");
+          }
+          if (Persons.affectedRows == 0) {
+            reject("No such word");
+          } else {
+            resolve("Edited word succesfully");
+          }
+        }
+      );
+    }),
 };
 
 export default Connections;
