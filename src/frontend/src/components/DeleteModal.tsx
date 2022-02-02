96a60d9f74141fe20ef841ba6cf84fe299cf4cee
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-02-02 19:43:30 +0200

    Documentation: DeleteModal.tsx

--- src/frontend/src/components/DeleteModal.tsx
+++ src/frontend/src/components/DeleteModal.tsx
@@ -1,11 +1,20 @@
 import { Dispatch, SetStateAction } from "react";
 import Person from "../Interfaces";
-
+/**
+ * This interface contains types of the props that DeleteModal uses
+ */
 interface Props {
   handleDelete: (person: Person) => void;
   deleteThis: Person;
   setDeleteModal: Dispatch<SetStateAction<boolean>>;
 }
+
+/**
+ * This component renders the delete modal to the screen that is called when the user clicks delete button from the person object.
+ * @param handleDelete is the delete function from the Persons.tsx that is called when deleting.
+ * @param deleteThis contains data of the person to be deleted.
+ * @returns a modal background and modal that contains buttons that call functions for handling deletion or returning.
+ */
 
 export const DeleteModal: React.FunctionComponent<Props> = ({
   handleDelete,
