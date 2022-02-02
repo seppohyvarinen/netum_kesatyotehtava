1707e71171b2e166c7ae48a8fde4cfa148cc15f6
Seppo Hyvärinen <seppo.hyvarinen@.tuni.fi>
2022-02-02 19:48:25 +0200

    Documentation: SearchBar.tsx

--- src/frontend/src/components/SearchBar.tsx
+++ src/frontend/src/components/SearchBar.tsx
@@ -1,13 +1,28 @@
 import { useState, useEffect } from "react";
 
+/**
+ * This interface contains props that the SearchBar component uses
+ */
+
 interface Props {
   filterPersons: (filterBy: string) => void;
 }
+
+/**
+ * This component renders searchbar that works in real time.
+ * @param filterPersons is the function from Persons.tsx that handles filtering the list that is rendered to the user.
+ * @returns A form that contains input for the user to type search parameters in to.
+ */
 
 export const SearchBar: React.FunctionComponent<Props> = ({
   filterPersons,
 }) => {
   const [searchParams, setSearchParams] = useState<string>("");
+
+  /**
+   * filterPersons function is called in useEffect when searchParams state is changed
+   * to ensure search is done in real time.
+   */
 
   useEffect(() => {
     filterPersons(searchParams);
@@ -16,8 +31,12 @@ export const SearchBar: React.FunctionComponent<Props> = ({
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("search functionality");
   };
+
+  /**
+   * Handles setting the searchParams state when user types.
+   * @param e is the React.FormEvent for HTML input element
+   */
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setSearchParams(e.currentTarget.value);
